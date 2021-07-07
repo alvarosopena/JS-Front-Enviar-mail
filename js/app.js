a52a7d15e86a28ffcb0434d4f0242fd3ce257f0e
alvarosopena <alvarosopenaf@gmail.com>
2021-07-06 21:16:33 -0300

    JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 //EL PROYECTO ESTA HECHO CON EL FRAMEWORk TAILWIND
 
 //Variables
-const btnEnviar = document.querySelector("#enviar")
-const formulario = document.querySelector("#enviar-mail")
-const btnReset = document.querySelector("#resetBtn")
+const btnEnviar = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
+const btnReset = document.querySelector("#resetBtn");
 
 //Variables para campos
-const email = document.querySelector("#email")
-const asunto = document.querySelector("#asunto")
-const mensaje = document.querySelector("#mensaje")
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
 
 //para validar el mail hacemos una variable global. Si la ponemos dentro del scope de validarmail no podremos usarla despues
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -17,15 +17,15 @@ eventListeners();
 
 function eventListeners(){
     //cuando la app arranca
-    document.addEventListener("DOMContentLoaded", iniciarApp)
+    document.addEventListener("DOMContentLoaded", iniciarApp);
 
     //campos del formulario
-    email.addEventListener("blur", validarFormulario)
-    asunto.addEventListener("blur", validarFormulario)
-    mensaje.addEventListener("blur", validarFormulario)
+    email.addEventListener("blur", validarFormulario);
+    asunto.addEventListener("blur", validarFormulario);
+    mensaje.addEventListener("blur", validarFormulario);
 
     //reinicia el formulario
-    btnReset.addEventListener("click", resetearFormulario)
+    btnReset.addEventListener("click", resetearFormulario);
 
     //enviar email
     formulario.addEventListener("submit", enviarEmail);
@@ -35,14 +35,14 @@ function eventListeners(){
 
 //Funciones
 function iniciarApp(){
-    /* pongo en disablen y en gris el boton de enviar */
+    /* pongo en disabled y en gris el boton de enviar */
     btnEnviar.disabled = true;
     btnEnviar.classList.add("cursor-not-allowed" , "opacity-50")
 }
 
 
 //valida el formulario
-function validarFormulario(e){
+function validarFormulario(e) {
     
    if(e.target.value.length > 0){
 
@@ -67,7 +67,7 @@ function validarFormulario(e){
         //expresion regular - busca nombre arroba y dominio
         
         /* const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; */
-        /* la sbubimos y ponemos como variable global para poder usarla en otros lados*/
+        /* la subimos y ponemos como variable global para poder usarla en otros lados*/
         
         if (re.test ( e.target.value ) ){
             //elimina los errores
@@ -148,8 +148,12 @@ function enviarEmail (e){
 
 }
 
+
 //funcion que resetea el form
-function resetearFormulario(){
+function resetearFormulario(e){
+     
+    e.preventDefault();
+    
     formulario.reset();
     
     iniciarApp();
